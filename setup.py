e3e75bf20f372cd4191b47296517b86feb8fa8a3
Sadaf Shafi <45829755+SadafShafi@users.noreply.github.com>
2020-10-02 12:33:25 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+
+from distutils.core import setup
+setup(
+  name = 'GROUP',        
+  packages = ['GROUP'], 
+  version = '0.1',     
+  license='MIT',        
+  description = 'A data structure called group',   
+  author = 'Sadaf Shafi',                  
+  author_email = 'daee.sadaf2011@gmail.com',      
+  url = 'https://github.com/SadafShafi/GROUP',   
+  download_url = 'https://github.com/SadafShafi/GROUP.git',
+  install_requires=[ 'multipledispatch'
+      ]
+      )
